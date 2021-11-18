d6cd010f32cd17c5ce2c89161b31d595425b4e97
veriano <veriano@ukr.net>
2021-11-18 19:02:27 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 import './css/styles.css';
+import Notiflix from 'notiflix';
 const debounce = require('lodash.debounce');
+import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+
+const input = document.getElementById('search-box');
+const countryList = document.querySelector('country-list');
+const countryInfo = document.querySelector('country-info');
+input.addEventListener('input', debounce(onInputEvent, DEBOUNCE_DELAY));
+
+function onInputEvent(e) {
+    if (input.value === '') {
+        
+    }
+};
+
+
